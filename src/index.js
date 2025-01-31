bfbd581fe5e18ed6412e9322ef2fdf0296ca43e2
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-01-31 11:44:14 +0530

    Shifted DB from local to prod

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 // connectToMongo().then(() => { console.log("Successfully connected to database") });
 
 const PORT = process.env.PORT || 8080
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/"
+const dbUrl = process.env.PROD_URI || "mongodb://localhost:27017/"
 
 console.log(dbUrl)
 
